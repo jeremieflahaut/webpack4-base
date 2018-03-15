2ab48a22163efbc7884956d6cd9764a2fe17f008
Jeremie <jeremieflahaut@gmail.com>
2018-03-15 20:04:18 +0100

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 const path = require('path')
+const webpack = require('webpack');
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: './js/index.js'
+  entry: './js/index.js',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 9000
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }
